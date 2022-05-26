40d679954536d5976e5ea3990ca94a2e7dbc4942
Your Name <nithyaramadoz@gmail.com>
2022-05-26 14:19:39 +0530

    Added: Lambok Library for auto generate getters and setters for Logging

--- src/main/java/com/example/addressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/example/addressbookapp/dto/AddressBookDTO.java
@@ -1,6 +1,8 @@
 package com.example.addressbookapp.dto;
 
-public class AddressBookDTO {
+import lombok.Data;
+
+public @Data class AddressBookDTO {
     public String firstName;
     public String lastName;
     public String address;
@@ -21,66 +23,5 @@ public class AddressBookDTO {
         this.phone = phone;
     }
 
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getZip() {
-        return zip;
-    }
-
-    public void setZip(String zip) {
-        this.zip = zip;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    @Override
-    public String toString() {
-        return "AddressBookDTO [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", state="
-                + state + ", city=" + city + ", zip=" + zip + ", phone=" + phone + "]";
-    }
 
 }
