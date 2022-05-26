cc00e17615d1e2a7cdfddc843e96463017dbe914
Your Name <nithyaramadoz@gmail.com>
2022-05-26 16:08:57 +0530

    Added: custom Exception Handling

--- src/main/java/com/example/addressbookapp/dto/AddressBookDTO.java
+++ src/main/java/com/example/addressbookapp/dto/AddressBookDTO.java
@@ -6,20 +6,20 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 public @Data class AddressBookDTO {
-    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Address book is Invalid!!! ")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "First name is Invalid!!! ")
     @NotEmpty(message = "first name cannot be null")
     public String firstName;
-    @Pattern(regexp = "^[A-Z]{1,}[a-zA-z\\s]{2,}$", message = "Address book is Invalid!!! ")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-z\\s]{2,}$", message = "Last name is Invalid!!! ")
     @NotEmpty(message = "last name cannot be null")
     public String lastName;
-    @Pattern(regexp = "^[A-Za-z,.0-9]{3,}$", message = "Address is Invalid!!! ")
+    @Pattern(regexp = "^[A-Za-z,.0-9\\s]{3,}$", message = "Address is Invalid!!! ")
     @NotEmpty(message = "Address cannot be null")
     public String address;
     public String state;
     public String city;
     @Pattern(regexp = "^[0-9]{3}\\s{0,1}[0-9]{3}$")
     public String zip;
-    @Pattern(regexp = "^^[0-9]{2}?[\\s,-]{0,1}[7-9]{1}[0-9]{9}$")
+    @Pattern(regexp = "^[0-9]{2}?[\\s,-]{0,1}[7-9]{1}[0-9]{9}$",message = "Phone number is Invalid")
     public String phone;
 
     public AddressBookDTO(String firstName, String lastName, String address, String state, String city, String zip,
